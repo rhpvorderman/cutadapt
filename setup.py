588a6ff26f2f9f9c9055d0f666552ca43fab467f
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-03 07:40:47 +0200

    Compile qualtrim with avx2 support

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import setuptools_scm  # noqa  Ensure it’s installed
 
 extensions = [
     Extension("cutadapt._align", sources=["src/cutadapt/_align.pyx"]),
-    Extension("cutadapt.qualtrim", sources=["src/cutadapt/qualtrim.pyx"]),
+    Extension("cutadapt.qualtrim", sources=["src/cutadapt/qualtrim.pyx"],
+              extra_compile_args=["-mavx2"]),
     Extension("cutadapt.info", sources=["src/cutadapt/info.pyx"]),
     Extension("cutadapt._kmer_finder", sources=["src/cutadapt/_kmer_finder.pyx"]),
 ]
